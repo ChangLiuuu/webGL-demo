4b9c7e7aaff05eefc51f5f24716dec64c2aa2b27
ChangLiuuu <spiritchang@hotmail.com>
2017-10-03 21:41:07 -0700

    slider

--- midpoint_circle.js
+++ midpoint_circle.js
@@ -16,13 +16,14 @@ var InitDemo = function() {
     }
 
 
-    /* 从像素坐标转换到 1.0 --> 再把 0->1 转换 0->2 -- >  把 0->2 转换到 -1->+1 (裁剪空间) */
+    // mapping from resolution to -1 +1 clip space
     var vertexShaderStr = "\
     attribute vec2 a_position;\
     uniform vec2 u_resolution;\
     void main() {\
         vec2 trans = a_position / u_resolution;\
         gl_Position = vec4(trans, 0, 1);\
+        gl_PointSize = 1.0;\
     }\
     ";
 
@@ -47,6 +48,8 @@ var InitDemo = function() {
         alert(gl.getShaderInfoLog(fragmentShader));
     }
 
+
+    /* create GLSL program and bind shader */
     var prg = gl.createProgram();
     gl.attachShader(prg, vertexShader);
     gl.attachShader(prg, fragmentShader);
@@ -57,13 +60,13 @@ var InitDemo = function() {
         gl.deleteProgram(prg);
     }
 
-    /* look up where the vertex data needs to go. */
+    // look up where the vertex data needs to go.
     var positionAttributeLocation = gl.getAttribLocation(prg, 'a_position');
 
-    /* look up uniform locations */
+    // look up uniform locations
     var resolutionUniformLocation = gl.getUniformLocation(prg, 'u_resolution');
 
-    /* create a buffer to put 2d clip space points in */
+    // create a buffer to put 2d clip space points in cause attributes get their data from buffers
     var positionBuffer = gl.createBuffer();
     if (!positionBuffer) {
         alert("Unable to initialize the vertexBuffer.");
@@ -75,48 +78,38 @@ var InitDemo = function() {
 
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-    /* convert from clip space values to back into pixels(screen space)  */
-    //This tells WebGL the -1 +1 clip space maps to 0 <-> gl.canvas.width for x and 0 <-> gl.canvas.height for y.
-    // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    /* clear the canvas */
+    // clear the canvas
     gl.clearColor(175 / 225, 240 / 225, 245 / 225, 1.0);// set canvas's color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    /* tell WebGL which shader program to execute */
+    // tell WebGL which shader program to execute
     gl.useProgram(prg);
 
-    /* first tell WebGL how to take data from the buffer we setup above and supply it to the attribute in the shader */
-
-    /* then need to specify how to pull the data out */
 
     // Bind the position buffer.
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
+    /* put into data */
+    var positions = midPoint(200).pointArr;
 
-    var positions = midPoint(100).pointArr;
-
+    // put data in that buffer by referencing it through the bind point
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-
-    // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    // Clear the canvas
+    // clear the canvas
     gl.clearColor(175 / 225, 240 / 225, 245 / 225, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Tell it to use our program (pair of shaders)
+    // tell it to use our program (pair of shaders)
     gl.useProgram(prg);
 
-    // Turn on the attribute
+    // turn on the attribute
     gl.enableVertexAttribArray(positionAttributeLocation);
 
-    // Bind the position buffer.
+    // bind the position buffer.
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-    var size = 2;          // 2 components per iteration
+    // tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration, MUST be 1,2,3 or 4
     var type = gl.FLOAT;   // the data is 32bit floats
     var normalize = false; // don't normalize the data
     var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
@@ -134,6 +127,7 @@ var InitDemo = function() {
     gl.drawArrays(primitiveType, offset2, count);
 };
 
+// midpoint circle algorithm
 var midPoint = function(r) {
     var x = 0;
     var y = r;
